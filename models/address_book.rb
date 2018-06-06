97e1a6a3b86d1e4bd56f0c3bd34ea327348be7c9
ben7ny <benjason777@gmail.com>
2018-06-06 00:28:13 -0400

    some problem with menu

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,14 @@
 require_relative 'entry'
 
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new
+ # #5
+ system "clear"
+ puts "Welcome to AddressBloc!"
+ # #6
+ menu.main_menu
+
    class AddressBook
      attr_reader :entries
 
